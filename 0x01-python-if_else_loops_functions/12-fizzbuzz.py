df68f9b2ac3a94dfb5e58f6758bde1e37452034c
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-08-08 07:12:26 -0700

    kfjskldf

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in range(101):
+    for n in range(1, 101):
         if n == 100:
             print('Buzz')
         elif n % 3 == 0 and n % 5 != 0:
-            print('Fizz ')
+            print('Fizz', end=" ")
         elif n % 5 == 0 and n % 3 != 0:
-            print('Buzz ')
+            print('Buzz', end=" ")
         elif n % 3 == 0 and n % 5 == 0:
-            print('FizzBuzz ')
+            print('FizzBuzz', end=" ")
+        else:
+            print(n, end=" ")
+
+fizzbuzz()
 
