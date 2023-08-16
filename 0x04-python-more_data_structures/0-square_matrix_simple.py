faba3c0b89b370c186a24cdb7ad30cd724f0e01a
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-08-16 05:34:53 -0700

    python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [];
+        for el in row:
+            new_row.append(el * el)
+        new_matrix.append(new_row)
+    return new_matrix
