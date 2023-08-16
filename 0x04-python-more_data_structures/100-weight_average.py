7b04f51bf81b8ee39fd28087d609843540442259
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-08-16 09:15:19 -0700

    python

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     new_list = list(map(lambda item: (item[0] * item[1], item[1]), my_list))
     score = 0
     weight = 0
