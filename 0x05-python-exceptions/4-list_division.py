14c833607c690b99c0a7fcaa3dc1f9bb6d4dc2a2
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2023-08-28 11:55:51 +0300

    editing vsc

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for i in range(list_length):
-            if my_list_1[i] == 0 or my_list_2[i] == 0:
-                raise ZeroDivisionError
-            elif (type(my_list_1[i]) == int or type(my_list_1[i]) == float) and (type(my_list_2[i]) == int or type(my_list_2[i] == float)):
-                a = my_list_1[i] / my_list_2[i]
-                new_list.append(a)
-            else:
-                raise TypeError
-    except ZeroDivisionError:
-        print("division by 0")
-        new_list.append(0)
-        continue
-    except TypeError:
-        print("wrong type")
-        new_list.append(0)
-    except IndexError:
-        print("out of range")
-        new_list.append(0)
-    finally:
-        return new_list
+    for i in range(list_length):
+        try:
+            a = my_list_1[i] / my_list_2[i]
+            new_list.append(a)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+            continue
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+            continue
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+            continue
+        finally:
+            return new_list
