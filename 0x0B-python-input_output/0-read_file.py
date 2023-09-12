3e37f3cfac06185b014bbe45025d07c5e43de14a
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-09-12 12:29:56 +0300

    fix read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ A function that read a file after openning it. """
     with open(filename, encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
