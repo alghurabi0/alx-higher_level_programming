bf5388c86ba30a68f591ff30eaef3aba509d1bba
root <root@DESKTOP-72J3LMK>
2023-10-17 20:13:25 +0300

    fixes

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,4 @@
 -- List all tables in the specified database
 
 -- This query retrieves the table names
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE(); -- This uses the currently selected database
-
--- If you want to use a specific database name provided as an argument to the mysql command, you can remove the DATABASE() function and use the following instead:
--- WHERE table_schema = 'your_database_name';
+SHOW tables;
