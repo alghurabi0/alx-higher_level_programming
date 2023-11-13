dc276135347a9154e71f960ef073d812497dddca
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-13 17:55:38 +0000

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv) {
-  if (process.argv.length <= 2) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument Found');
-  } else {
-    console.log('Arguments Found');
-  }
-} else {
+const numArguments = process.argv.length - 2;
+
+if (numArguments === 0) {
   console.log('No argument');
+} else if (numArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
