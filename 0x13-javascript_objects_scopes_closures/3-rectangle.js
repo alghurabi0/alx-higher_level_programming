7cef04baf4d4ef0d4e6926f86cf4cb1711f62f98
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-14 13:52:50 +0000

    print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,6 +8,17 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    let h, w;
+    let line = '';
+    for (w = 0; w < this.width; w++) {
+      line += 'X';
+    }
+    for (h = 0; h < this.height; h++) {
+      console.log(line);
+    }
+  }
 }
 
 module.exports = Rectangle;
