30698f3e25181a269acb5f7ced285bc25706d204
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-18 00:06:20 +0000

    tas eight

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Script that lists all State objects from the database hbtn_0e_6_usa
+"""Script that prints the first State object from the database hbtn_0e_6_usa
 """
 
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -22,8 +21,10 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    first_state = session.query(State).order_by(State.id).first()
 
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
     session.close()
