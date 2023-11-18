85f1e1f67ae8f267831a475e493bcb1e163f85dd
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-18 00:56:36 +0000

    fdjsf

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,20 +5,18 @@
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                 sys.argv[1],
                 sys.argv[2],
                 sys.argv[3]
-                ),
-            pool_pre_ping=True
-            )
+                ))
 
-    Session = Session(bind=engine)
+    Session = sessionmaker(bind=engine)
 
     session = Session()
 
